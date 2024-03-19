e13bab3282ffc9e55049d612146daf2ba2a647f3
ATOM03 <priyanshujhaginia@outlook.com>
2024-03-19 19:55:48 +0530

    Working on the Array.

---
+++ src/Arrays/Maximum_SubArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Maximum_SubArray {
+    public static void main(String[] args) {
+        int ar[]={-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(maxSubarray(ar));
+    }
+
+    private static int maxSubarray(int[] ar) {
+        int n= ar.length;
+        int max_so_far=Integer.MIN_VALUE;
+        int max_sum=0;
+
+        for(int i =0;i<n;i++){
+            max_sum+=ar[i];
+            if(max_so_far<max_sum)
+                max_so_far=Math.max(max_sum,max_so_far);
+            if(max_sum<0)
+                max_sum=0;
+        }
+        return max_so_far;
+    }
+}
