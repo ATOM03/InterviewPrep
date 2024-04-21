bc90bca3bf3b70f551d2366e90c28ec8c94f2f40
ATOM03 <priyanshujhaginia@outlook.com>
2024-04-21 23:29:34 +0530

    Working on the Linkedlist.

--- src/DesignStructures/LinkedList.java
+++ src/DesignStructures/LinkedList.java
@@ -112,6 +112,19 @@ public class LinkedList {
 
 
     }
+    public boolean hasCycle(Node head){
+        Node slow=head;
+        Node  fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         LinkedList list=new LinkedList();
 
